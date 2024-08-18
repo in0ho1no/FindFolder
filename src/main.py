4d4eaeefe38c0dd850e10b92bb717cab8fdd1b94
in0ho1no <yin0ho1no@gmail.com>
2024-08-19 04:04:26 +0900

    ヘッダクリックによりファイル一覧を並び替えるよう対応

--- src/main.py
+++ src/main.py
@@ -3,11 +3,159 @@ import os
 import tkinter as tk
 from enum import Enum, unique
 from tkinter import filedialog, messagebox, ttk
-from typing import List, Literal
 
 
 class FolderExplorerApp:
     """GUI作成"""
+
+    class FolderTreeview:
+        """フォルダを一覧表示するTreeview"""
+
+        @unique
+        class ColList(Enum):
+            DATE = 0
+            TYPE = 1
+            VER = 2
+
+        COL_REVERSE_DEFAULT = False
+        COL_DICT_LIST: list[dict] = [
+            {  # ColList.DATE
+                "ID": "ID_Date",
+                "NAME": "Data",
+                "ANCHOR": "w",
+                "WIDTH": 120,
+                "REVERSE": COL_REVERSE_DEFAULT,
+            },
+            {  # ColList.TYPE
+                "ID": "ID_Type",
+                "NAME": "Type",
+                "ANCHOR": "w",
+                "WIDTH": 100,
+                "REVERSE": COL_REVERSE_DEFAULT,
+            },
+            {  # ColList.VER
+                "ID": "ID_Ver",
+                "NAME": "Ver",
+                "ANCHOR": "w",
+                "WIDTH": 60,
+                "REVERSE": COL_REVERSE_DEFAULT,
+            },
+        ]
+
+        def __init__(self, frame_r: ttk.Frame) -> None:
+            self.frame_m = frame_r
+
+            # フォルダを一覧表示する
+            self.treeview_folder = ttk.Treeview(self.frame_m)
+            self.treeview_folder.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+            # 列名の定義
+            self.treeview_folder["columns"] = (
+                self.COL_DICT_LIST[self.ColList.DATE.value]["ID"],
+                self.COL_DICT_LIST[self.ColList.TYPE.value]["ID"],
+                self.COL_DICT_LIST[self.ColList.VER.value]["ID"],
+            )
+
+            # 列の定義
+            self.treeview_folder.column("#0", width=0, stretch=False)  # 不要な0列目を非表示
+
+            self.treeview_folder.column(
+                self.COL_DICT_LIST[self.ColList.DATE.value]["ID"],
+                anchor=self.COL_DICT_LIST[self.ColList.DATE.value]["ANCHOR"],
+                width=self.COL_DICT_LIST[self.ColList.DATE.value]["WIDTH"],
+            )
+            self.treeview_folder.column(
+                self.COL_DICT_LIST[self.ColList.TYPE.value]["ID"],
+                anchor=self.COL_DICT_LIST[self.ColList.TYPE.value]["ANCHOR"],
+                width=self.COL_DICT_LIST[self.ColList.TYPE.value]["WIDTH"],
+            )
+            self.treeview_folder.column(
+                self.COL_DICT_LIST[self.ColList.VER.value]["ID"],
+                anchor=self.COL_DICT_LIST[self.ColList.VER.value]["ANCHOR"],
+                width=self.COL_DICT_LIST[self.ColList.VER.value]["WIDTH"],
+            )
+
+            # ヘッダの作成
+            self.treeview_folder.heading(
+                self.COL_DICT_LIST[self.ColList.DATE.value]["ID"],
+                text=self.COL_DICT_LIST[self.ColList.DATE.value]["NAME"],
+                anchor=self.COL_DICT_LIST[self.ColList.DATE.value]["ANCHOR"],
+                command=lambda: self.treeview_header_click(self.ColList.DATE.value),
+            )
+            self.treeview_folder.heading(
+                self.COL_DICT_LIST[self.ColList.TYPE.value]["ID"],
+                text=self.COL_DICT_LIST[self.ColList.TYPE.value]["NAME"],
+                anchor=self.COL_DICT_LIST[self.ColList.TYPE.value]["ANCHOR"],
+                command=lambda: self.treeview_header_click(self.ColList.TYPE.value),
+            )
+            self.treeview_folder.heading(
+                self.COL_DICT_LIST[self.ColList.VER.value]["ID"],
+                text=self.COL_DICT_LIST[self.ColList.VER.value]["NAME"],
+                anchor=self.COL_DICT_LIST[self.ColList.VER.value]["ANCHOR"],
+                command=lambda: self.treeview_header_click(self.ColList.VER.value),
+            )
+
+            # フォルダ一覧にスクロールバーを追加する
+            scrollbar_list = tk.Scrollbar(self.frame_m, orient=tk.VERTICAL, command=self.treeview_folder.yview)
+            scrollbar_list.pack(side=tk.RIGHT, fill=tk.Y)
+            self.treeview_folder.config(yscrollcommand=scrollbar_list.set)
+
+        def update_folder_list(self, folder_path_list_r: list[str]) -> None:
+            """フォルダ一覧の更新
+
+            Args:
+                folder_path_list_r (list[str]): フォルダパスのリスト
+            """
+
+            # 一覧を全削除する
+            for treeview_child in self.treeview_folder.get_children():
+                self.treeview_folder.delete(treeview_child)
+
+            # 並び替え指定を初期化する
+            for col_dict in self.COL_DICT_LIST:
+                col_dict["REVERSE"] = self.COL_REVERSE_DEFAULT
+
+            # フォルダパスをパースして表示する
+            for folder_path in folder_path_list_r:
+                dir_name = folder_path.split(os.sep)[-2]
+                date, type, var = dir_name.split("_")
+                self.treeview_folder.insert(
+                    parent="",
+                    index="end",
+                    values=(
+                        date,
+                        type,
+                        var,
+                    ),
+                )
+
+            # 初回の並び替えを行う
+            self.sort_column(self.ColList.DATE.value)
+
+        def sort_column(self, col_num_r: int) -> None:
+            """指定された列に応じてリストを並び替える"""
+
+            # リストの要素取得
+            folder_list = self.treeview_folder.get_children()
+            if 0 == len(folder_list):
+                return
+
+            # リストの並び替え
+            data_list = [(self.treeview_folder.set(folder, self.COL_DICT_LIST[col_num_r]["ID"]), folder) for folder in folder_list]
+            data_list.sort(reverse=self.COL_DICT_LIST[col_num_r]["REVERSE"])
+            for index, (_, item) in enumerate(data_list):
+                self.treeview_folder.move(item, "", index)
+
+            # 次回に向けて並び替え指定を更新
+            self.COL_DICT_LIST[col_num_r]["REVERSE"] = not self.COL_DICT_LIST[col_num_r]["REVERSE"]
+
+        def treeview_header_click(self, col_num_r: int) -> None:
+            """ヘッダクリック時のイベントを行う
+
+            Args:
+                col_num_r (int): クリックされた列番号
+            """
+            self.sort_column(col_num_r)
 
     def __init__(self, root: tk.Tk) -> None:
         self.root_m = root
@@ -29,101 +177,7 @@ class FolderExplorerApp:
         self.btn_folder_open_m.pack(fill=tk.BOTH, expand=False)
 
         # フォルダ一覧を表示する
-        folder_treeview = self.FolderTreeview(self.frame_m)
-        self.folder_treeview_m = folder_treeview.create_folder_treeview()
-
-    class FolderTreeview:
-        """フォルダを一覧表示するTreeview"""
-
-        @unique
-        class ColList(Enum):
-            DATE = 0
-            TYPE = 1
-            VER = 2
-
-        COL_DICT_LIST: list[dict] = [
-            {  # ColList.DATE
-                "ID": "ID_Date",
-                "NAME": "Data",
-                "ANCHOR": "w",
-                "WIDTH": 120,
-            },
-            {  # ColList.TYPE
-                "ID": "ID_Type",
-                "NAME": "Type",
-                "ANCHOR": "w",
-                "WIDTH": 100,
-            },
-            {  # ColList.VER
-                "ID": "ID_Ver",
-                "NAME": "Ver",
-                "ANCHOR": "w",
-                "WIDTH": 60,
-            },
-        ]
-
-        def __init__(self, frame_r: ttk.Frame) -> None:
-            self.frame_m = frame_r
-
-        def create_folder_treeview(self) -> ttk.Treeview:
-            # フォルダを一覧表示する
-            self.treeview_folder = ttk.Treeview(self.frame_m)
-            self.treeview_folder.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-            # 列名の定義
-            self.treeview_folder["columns"] = (
-                self.COL_DICT_LIST[self.ColList.DATE.value]["ID"],
-                self.COL_DICT_LIST[self.ColList.TYPE.value]["ID"],
-                self.COL_DICT_LIST[self.ColList.VER.value]["ID"],
-            )
-
-            # 列の定義
-            col_num = self.ColList.DATE.value
-            self.treeview_folder.column("#0", width=0, stretch=False)  # 不要な0列目を非表示
-            self.treeview_folder.column(
-                self.COL_DICT_LIST[col_num]["ID"],
-                anchor=self.COL_DICT_LIST[col_num]["ANCHOR"],
-                width=self.COL_DICT_LIST[col_num]["WIDTH"],
-            )
-            col_num = self.ColList.TYPE.value
-            self.treeview_folder.column(
-                self.COL_DICT_LIST[col_num]["ID"],
-                anchor=self.COL_DICT_LIST[col_num]["ANCHOR"],
-                width=self.COL_DICT_LIST[col_num]["WIDTH"],
-            )
-            col_num = self.ColList.VER.value
-            self.treeview_folder.column(
-                self.COL_DICT_LIST[col_num]["ID"],
-                anchor=self.COL_DICT_LIST[col_num]["ANCHOR"],
-                width=self.COL_DICT_LIST[col_num]["WIDTH"],
-            )
-
-            # ヘッダの作成
-            col_num = self.ColList.DATE.value
-            self.treeview_folder.heading(
-                self.COL_DICT_LIST[col_num]["ID"],
-                text=self.COL_DICT_LIST[col_num]["NAME"],
-                anchor=self.COL_DICT_LIST[col_num]["ANCHOR"],
-            )
-            col_num = self.ColList.TYPE.value
-            self.treeview_folder.heading(
-                self.COL_DICT_LIST[col_num]["ID"],
-                text=self.COL_DICT_LIST[col_num]["NAME"],
-                anchor=self.COL_DICT_LIST[col_num]["ANCHOR"],
-            )
-            col_num = self.ColList.VER.value
-            self.treeview_folder.heading(
-                self.COL_DICT_LIST[col_num]["ID"],
-                text=self.COL_DICT_LIST[col_num]["NAME"],
-                anchor=self.COL_DICT_LIST[col_num]["ANCHOR"],
-            )
-
-            # フォルダ一覧にスクロールバーを追加する
-            scrollbar_list = tk.Scrollbar(self.frame_m, orient=tk.VERTICAL, command=self.treeview_folder.yview)
-            scrollbar_list.pack(side=tk.RIGHT, fill=tk.Y)
-            self.treeview_folder.config(yscrollcommand=scrollbar_list.set)
-
-            return self.treeview_folder
+        self.folder_treeview_m = self.FolderTreeview(self.frame_m)
 
     def open_folder(self) -> list[str]:
         """対象とするフォルダを選択する
@@ -146,35 +200,10 @@ class FolderExplorerApp:
 
         return folder_path_list
 
-    def update_folder_list(self, folder_path_list_r: list[str]) -> None:
-        """フォルダ一覧の更新
-
-        Args:
-            folder_path_list_r (list[str]): フォルダパスのリスト
-        """
-
-        # 一覧を全削除する
-        for treeview_child in self.folder_treeview_m.get_children():
-            self.folder_treeview_m.delete(treeview_child)
-
-        # フォルダパスをパースして表示する
-        for folder_path in folder_path_list_r:
-            dir_name = folder_path.split(os.sep)[-2]
-            date, type, var = dir_name.split("_")
-            self.folder_treeview_m.insert(
-                parent="",
-                index="end",
-                values=(
-                    date,
-                    type,
-                    var,
-                ),
-            )
-
     def btn_folder_open_click(self) -> None:
         """指定フォルダ内のフォルダ一覧を表示する"""
         folder_path_list = self.open_folder()
-        self.update_folder_list(folder_path_list)
+        self.folder_treeview_m.update_folder_list(folder_path_list)
 
 
 def main() -> None:
