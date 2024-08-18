804a6cdf18ac532b43588d74296128b90cd026f6
in0ho1no <yin0ho1no@gmail.com>
2024-08-19 02:31:36 +0900

    フォルダ名をパースしながら一覧表示するように対応

--- src/main.py
+++ src/main.py
@@ -1,38 +1,115 @@
 import glob
 import os
 import tkinter as tk
+from enum import Enum, unique
 from tkinter import filedialog, messagebox, ttk
+from typing import List, Literal
 
 
 class FolderExplorerApp:
+    @unique
+    class ColList(Enum):
+        DATE = 0
+        TYPE = 1
+        VER = 2
+
+    COL_DICT_LIST: list[dict] = [
+        {  # ColList.DATE
+            "ID": "ID_Date",
+            "NAME": "Data",
+            "ANCHOR": "w",
+            "WIDTH": 120,
+        },
+        {  # ColList.TYPE
+            "ID": "ID_Type",
+            "NAME": "Type",
+            "ANCHOR": "w",
+            "WIDTH": 100,
+        },
+        {  # ColList.VER
+            "ID": "ID_Ver",
+            "NAME": "Ver",
+            "ANCHOR": "w",
+            "WIDTH": 60,
+        },
+    ]
+
     def __init__(self, root: tk.Tk) -> None:
         """GUI作成"""
         self.root_m = root
         self.root_m.title("FindFolder")
 
         # ウィンドウのサイズと位置指定
-        size_width_def = 300
-        size_height_def = 100
+        size_width_def = 400
+        size_height_def = 300
         pos_x_def = 10
         pos_y_def = 10
         self.root_m.geometry(f"{size_width_def}x{size_height_def}+{pos_x_def}+{pos_y_def}")
 
-        # Frame for Listbox
+        # 表示するためのframe
         frame = ttk.Frame(self.root_m)
         frame.pack(fill=tk.BOTH, expand=True)
 
         # フォルダを指定するボタン
         self.btn_folder_open = tk.Button(frame, text="フォルダを開く", command=self.btn_folder_open_click)
-        self.btn_folder_open.pack(fill=tk.BOTH, expand=True)
+        self.btn_folder_open.pack(fill=tk.BOTH, expand=False)
 
-        # フォルダを一覧表示するlistbox
-        self.listbox_folder = tk.Listbox(frame, selectmode=tk.SINGLE)
-        self.listbox_folder.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        # フォルダを一覧表示する
+        self.treeview_folder = ttk.Treeview(frame)
+        self.treeview_folder.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-        # listboxにスクロールバーを追加する
-        self.scrollbar_list = tk.Scrollbar(frame, orient=tk.VERTICAL, command=self.listbox_folder.yview)
+        # 列名の定義
+        self.treeview_folder["columns"] = (
+            self.COL_DICT_LIST[self.ColList.DATE.value]["ID"],
+            self.COL_DICT_LIST[self.ColList.TYPE.value]["ID"],
+            self.COL_DICT_LIST[self.ColList.VER.value]["ID"],
+        )
+
+        # 列の定義
+        col_num = self.ColList.DATE.value
+        self.treeview_folder.column("#0", width=0, stretch=False)  # 不要な0列目を非表示
+        self.treeview_folder.column(
+            self.COL_DICT_LIST[col_num]["ID"],
+            anchor=self.COL_DICT_LIST[col_num]["ANCHOR"],
+            width=self.COL_DICT_LIST[col_num]["WIDTH"],
+        )
+        col_num = self.ColList.TYPE.value
+        self.treeview_folder.column(
+            self.COL_DICT_LIST[col_num]["ID"],
+            anchor=self.COL_DICT_LIST[col_num]["ANCHOR"],
+            width=self.COL_DICT_LIST[col_num]["WIDTH"],
+        )
+        col_num = self.ColList.VER.value
+        self.treeview_folder.column(
+            self.COL_DICT_LIST[col_num]["ID"],
+            anchor=self.COL_DICT_LIST[col_num]["ANCHOR"],
+            width=self.COL_DICT_LIST[col_num]["WIDTH"],
+        )
+
+        # ヘッダの作成
+        col_num = self.ColList.DATE.value
+        self.treeview_folder.heading(
+            self.COL_DICT_LIST[col_num]["ID"],
+            text=self.COL_DICT_LIST[col_num]["NAME"],
+            anchor=self.COL_DICT_LIST[col_num]["ANCHOR"],
+        )
+        col_num = self.ColList.TYPE.value
+        self.treeview_folder.heading(
+            self.COL_DICT_LIST[col_num]["ID"],
+            text=self.COL_DICT_LIST[col_num]["NAME"],
+            anchor=self.COL_DICT_LIST[col_num]["ANCHOR"],
+        )
+        col_num = self.ColList.VER.value
+        self.treeview_folder.heading(
+            self.COL_DICT_LIST[col_num]["ID"],
+            text=self.COL_DICT_LIST[col_num]["NAME"],
+            anchor=self.COL_DICT_LIST[col_num]["ANCHOR"],
+        )
+
+        # フォルダ一覧にスクロールバーを追加する
+        self.scrollbar_list = tk.Scrollbar(frame, orient=tk.VERTICAL, command=self.treeview_folder.yview)
         self.scrollbar_list.pack(side=tk.RIGHT, fill=tk.Y)
-        self.listbox_folder.config(yscrollcommand=self.scrollbar_list.set)
+        self.treeview_folder.config(yscrollcommand=self.scrollbar_list.set)
 
     def open_folder(self) -> list[str]:
         """対象とするフォルダを選択する
@@ -48,7 +125,7 @@ class FolderExplorerApp:
             return []
 
         # 指定されたフォルダ内のフォルダ一覧を取得する
-        folder_path_list = glob.glob(f"{folder_path}\\**\\")
+        folder_path_list = glob.glob(f"{folder_path}{os.sep}**{os.sep}")
         if 0 == len(folder_path_list):
             messagebox.showerror("エラー", "フォルダの中身が空です。")
             return []
@@ -62,12 +139,23 @@ class FolderExplorerApp:
             folder_path_list_r (list[str]): フォルダパスのリスト
         """
 
-        # listboxをクリアする
-        self.listbox_folder.delete(0, tk.END)
+        # 一覧を全削除する
+        for treeview_child in self.treeview_folder.get_children():
+            self.treeview_folder.delete(treeview_child)
 
         # listboxにフォルダパスを設定する
         for folder_path in folder_path_list_r:
-            self.listbox_folder.insert(tk.END, folder_path)
+            dir_name = folder_path.split(os.sep)[-2]
+            date, type, var = dir_name.split("_")
+            self.treeview_folder.insert(
+                parent="",
+                index="end",
+                values=(
+                    date,
+                    type,
+                    var,
+                ),
+            )
 
     def btn_folder_open_click(self) -> None:
         """指定フォルダ内のフォルダ一覧を表示する"""
